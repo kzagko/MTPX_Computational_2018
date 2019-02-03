088ac3d982a0cbbbeab7f03723c896f6928580ae
Kimon <kzagko@gmail.com>
2019-02-03 18:47:20 +0200

    added dataanalysis project2019 exercise 3

---
+++ DataAnalysis/Final_Project_2019/MeanP10.m
@@ -0,0 +1,22 @@
+function MeanP10(AM,BM,alpha)
+    nCount = length(AM(1,:));%get the number of countries
+    pCIs = zeros(nCount,2);
+    npCIs = zeros(nCount,2);
+    mpCIs = zeros(nCount,1);
+    mnpCIs = zeros(nCount,1);
+    
+    for i=1:nCount
+       [pCIs(i,:), npCIs(i,:)] = CIs(AM(:,i),BM(:,i),alpha);       
+    end
+ 
+    mpCIs = mean(pCIs,2);
+    mnpCIs = mean(npCIs,2);
+    figure();
+    errorbar(1:nCount,mpCIs,pCIs(:,1),pCIs(:,2),'o');
+    hold on;
+    errorbar(1:nCount,mnpCIs,npCIs(:,1),npCIs(:,2),'*');
+    title('Mean and CIs of avtivity');
+    ylabel('Relative mean PM10 value');
+    xlabel('Country code');
+    legend('Parametric','Non Parametric');
+end
